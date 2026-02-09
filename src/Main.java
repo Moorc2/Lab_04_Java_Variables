7b85241395dc06527ee283403ee746093d32d577
Charles Moore <moorc2@mail.uc.edu>
2026-02-09 11:06:40 -0500

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int intOperandA = 2;
+        int intOperandB = 5;
+        int intSum = 7;
+        int intProduct = 3;
+        int intDifference = 6;
+        int intQuotient = 9;
+        int intModulo = 1;
+
+        double doubleOperandA = 3.50; // This is a declaration and initial assignment
+        double doubleOperandB = 3.50;
+        double doubleSum = 3.50;
+        double doubleProduct = 3.50;
+        double doubleDifference = 3.50;
+        double doubleQuotient = 3.50;
+
+        // Create delcations and output strings for each:
+        // + - * / %
+
+        intSum = intOperandA + intOperandB; // Assignment uses arithmetic operator + / addition
+        intDifference = intOperandB - intOperandA; // Assignment uses arithmetic operator - / substraction
+
+
+
+
+
     }
 }
